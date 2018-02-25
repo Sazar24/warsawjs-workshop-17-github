8f72826124c40ff1a1a69dbfee59966b0b4f397d
Sazar24 <budrewicz.cezary@gmail.com>
2018-02-25 16:56:15 +0100

    changed name location on main branch - i want conflict when merging to it

--- src/components/SkiResorts.jsx
+++ src/components/SkiResorts.jsx
@@ -7,7 +7,7 @@ class App extends Component {
     this.state = {
       skiResorts: [
         {
-          name: 'Original city',
+          name: 'Nazwa zmieniona na mainie',
           height: '152 m',
           price: '121,99 PLN',
           opening_hours: '10:00-20:00',
